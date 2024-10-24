4699b09856404fd79eb5b65b26625dd3003ba501
wyn2004 <thang09052004@gmail.com>
2024-10-24 14:35:47 +0700

    ffix2

--- src/middlewares/PrivateRoute.jsx
+++ src/middlewares/PrivateRoute.jsx
@@ -3,39 +3,18 @@ import { Navigate } from "react-router-dom";
 import { useUserStore } from "../store/useUserStore";
 import { toast } from "react-toastify";
 
-// const PrivateRoute = ({ children, role }) => {
-//   const { isLoggedIn } = useUserStore();
-//   if (!isLoggedIn) {
-//     return <Navigate to="/public/login" />;
-//   }
-
-//   if (!children.type.name.toUpperCase().includes(role.toUpperCase())) {
-//     return <Navigate to={'/'} />;
-//   }
-
-//   return children;
-// };
-
 const PrivateRoute = ({ children, role }) => {
   const { isLoggedIn } = useUserStore();
-
   if (!isLoggedIn) {
     return <Navigate to="/public/login" />;
   }
 
-  // Kiểm tra an toàn để đảm bảo children là một component có type và name
-  if (children && children.type && children.type.name) {
-    if (!children.type.name.toUpperCase().includes(role.toUpperCase())) {
-      return <Navigate to="/" />;
-    }
-  } else {
-    // Nếu children không phải là component hoặc không có name (có thể là HTML element)
-    console.warn(
-      "Child component does not have a type or name. Skipping role check."
-    );
-  }
+  console.log(children.type.name);
 
-  return children;
+  if (children.type.name.toUpperCase().includes(role.toUpperCase())) {
+    return children;
+  }
+  return <Navigate to={"/"} />;
 };
 
 export default PrivateRoute;
